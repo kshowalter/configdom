6160d04a055e14f6932492a524e0712ef9bc45a2
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-05-25 17:03:03 -0400

    Major rebuild to allow for non-updating elements

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
- * @fileOverview A view renderer based on a simple configuration object.
- * @author Keith Showalter {@link https://github.com/kshowalter}
- * @version 0.1.0
- */
+* @fileOverview A view renderer based on a simple configuration object.
+* @author Keith Showalter {@link https://github.com/kshowalter}
+* @version 0.1.0
+*/
 
 
 /** @module SimpleDOM */
@@ -15,38 +15,79 @@ var $ = require('simpledom');
 * @param  {object} config ConfigDOM config object
 * @return {element} DOM Element
 */
-var mkDOM = function mkDOM(config){
-  var e = $(config.tag);
-  var _id = config._id || 'r';
-  config['data_id'] = _id;
+var mkDOM = function(config, oldConfig){
+  this.numElementsChanged++;
+  var _id = config._id;
+  var parent_id = _id.split('.').slice(0,-1).join('.');
 
-  for( var name in config){
-    //console.log(name);
-    if( name === 'text'){
-      e.text(config[name]);
-    } else if( name === 'children'){
-      config[name].forEach(function(child,i){
-        if( child !== undefined ){
-          if( child.constructor === Object ){ // type = 'config';
-            child._id = _id + '.' + i
-            e.append( mkDOM(child) );
-          } else if( child.constructor === String ){ // type = 'textNode';
-            child = document.createTextNode(child);
-            e.append( child );
-          } else if( child.constructor.prototype === HTMLElement ) { // type = 'element';
-            e.append( child );
-          }
-        }
-      });
-    } else if( name === 'append'){
-      config[name].forEach(function(elem){
-        e.append( elem );
-      });
-    } else if( name === 'textNode'){
-      e.append( config[name] );
-    } else {
-      e.attr(name, config[name]);
+  var dirty = false;
+  var dirtyChildren = false;
+  if( config !== oldConfig ){
+    dirty = true;
+    if( config['children'] ){
+      dirtyChildren = true;
     }
+  } else {
+    for( var name in config ){
+      if( config[name] !== oldConfig[name] ){
+        if( name === 'children' ){
+          dirtyChildren = true;
+        } else {
+          dirty = true;
+        }
+      }
+    }
+    if( config['children'] && config['children'].length !== oldConfig['children'].length ){
+      dirtyChildren = true;
+    }
+  }
+
+  if( dirty ){
+    var e = $(config.tag);
+    var oldE = this.elements[_id];
+    if( ! oldE ){
+      this.elements[parent_id].append( e );
+    } else {
+      oldE.elem.parentNode.replaceChild(e.elem, oldE.elem);
+    }
+    this.elements[_id] = e;
+    for( var name in config ){
+      if( name === 'children'){
+        continue;
+      } else if( name === 'text'){
+        e.text(config[name]);
+      } else if( name === 'append'){
+        config[name].forEach(function(elem){
+          e.append( elem );
+        });
+      } else if( name === 'textNode'){
+        e.append( config[name] );
+      } else {
+        e.attr(name, config[name]);
+      }
+    }
+
+  }
+
+  if( dirtyChildren ){
+    var e = this.elements[_id];
+    e.clear();
+
+    config['children'].forEach(function(child,i){
+      if( child !== undefined ){
+        if( child.constructor === Object ){
+          child._id = _id + '.' + i;
+
+          e.append( this.mkDOM(child, {} ));
+        } else if( child.constructor === String ){
+          child = document.createTextNode(child);
+          e.append( child );
+        } else if( child.constructor.prototype === HTMLElement ) {
+          e.append( child );
+        }
+      }
+    }, this);
+
   }
 
   return e;
@@ -55,22 +96,31 @@ var mkDOM = function mkDOM(config){
 
 
 /**
- * ConfigDOM constructor
- * @exports test
- * @constructor ConfigDOM
- * @param  {string} id id of the parent element
- * @return {object} ConfigDOM object
- */
+* ConfigDOM constructor
+* @exports test
+* @constructor ConfigDOM
+* @param  {string} id id of the parent element
+* @return {object} ConfigDOM object
+*/
 module.exports = function(id){
   //module.exports = function(id){ // DOM id
   var C = {
     anchorID: id,
-    dom: $(id),
-    load: function(config){
-      this.dom.clear();
-      this.dom.append( mkDOM(config) );
+    config: {},
+    elements: {
+      'r': $(id)
+    },
+    mkDOM: mkDOM,
+    numElementsChanged: 0,
+    load: function(newConfig){
+      if( newConfig.constructor === Object){}
+      newConfig._id = 'r.0';
+      this.numElementsChanged = 0;
+      this.mkDOM(newConfig, this.config);
+      console.log(this.numElementsChanged);
     }
-  }
+  };
+
 
   return C;
-}
+};
