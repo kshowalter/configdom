c96069e9f93fa14d55b313009ba6d97ba6d8c017
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-15 10:06:55 -0400

    Fixed SVG generation

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 /** @module SimpleDOM */
 var $ = require('simpledom');
+var SVG_attr = require('./svg');
+console.log(SVG_attr);
 
 
 var configChanged = function(newConfig, oldConfig){
@@ -24,27 +26,66 @@ var configChanged = function(newConfig, oldConfig){
 };
 
 var mkNode = function(config){
-  var node;
+  var sdom;
   if( config.constructor === Object ){ // CONFIG
-    node = $(config.tag);
+    sdom = $(config.tag, {namespaceURI: config.meta.namespaceURI});
     if( config.props ){
       for( var name in config.props ){
-        node.attr(name, config.props[name]);
+        sdom.attr(name, config.props[name]);
       }
     }
     if( config.text ){
-      node.text( config.text );
+      sdom.text( config.text );
     }
   } else if( config.constructor === String ){ // TEXT NODE
-    node = document.createTextNode(config);
+    sdom = document.createTextNode(config);
   } else if( config.constructor.prototype === HTMLElement || config instanceof SVGElement ) { // NODE ELEMENT
-    node = config;
+    sdom = config;
   } else {
     console.warn('node config not recognized:', config);
-    node = undefined;
+    sdom = undefined;
   }
-  return node;
-}
+  return sdom;
+};
+
+var SVGize = function(config){
+  console.log('svg', config);
+  config.props = config.props || {};
+
+  if( config.tag === 'svg' ){
+    config.props['xmlns'] = 'http://www.w3.org/2000/svg';
+    config.props['xmlns:xlink'] = 'http://www.w3.org/1999/xlink';
+  }
+
+  config.meta = config.meta || {};
+  config.meta.namespaceURI = 'http://www.w3.org/2000/svg';
+  config.meta.layer_attr = config.meta.layer_attr || SVG_attr.layer_attr;
+  config.meta.fonts = config.meta.fonts || SVG_attr.fonts;
+
+  config.meta.layerName = config.meta.layerName || 'base';
+  config.meta.fontName = config.meta.fontName || 'base';
+
+  if( ! (['svg'].indexOf(config.tag)+1) ){
+    var layer = config.meta.layer_attr[config.meta.layerName];
+    for( var name in layer ){
+      config.props[name] = layer[name];
+    }
+  }
+
+  if( ['text', 'textPath', 'title', 'tref', 'tspan'].indexOf(config.tag)+1 ){
+    var font = config.meta.fonts[config.meta.fontName];
+    for( var name in font ){
+      config.props[name] = font[name];
+    }
+  }
+
+  return config;
+};
+
+//if( isSVG ){
+//  props.xmlns = props.xmlns || 'http://www.w3.org/2000/svg';
+//  props['xmlns:xlink'] = props['xmlns:xlink'] || 'http://www.w3.org/1999/xlink';
+//}
 
 
 /**
@@ -53,31 +94,42 @@ var mkNode = function(config){
 * @param  {object} config ConfigDOM config object
 * @return {element} DOM Element
 */
-var mkDOM = function mkDOM(parentNode, newConfig, oldConfig){
+var mkDOM = function mkDOM(parentConfig, newConfig, oldConfig){
   //var _id = config._id;
+  //console.log(parentConfig.sdom.elem, parentConfig.sdom.elem.namespaceURI);
+  newConfig.meta = newConfig.meta || {};
+
+  console.log(newConfig);
+  if( newConfig.constructor === Object &&
+      ( newConfig.tag.toLocaleLowerCase()  === 'svg' ||
+        parentConfig.meta.namespaceURI === 'http://www.w3.org/2000/svg' )
+    ){
+    newConfig = SVGize(newConfig);
+    //console.log(newConfig);
+  }
+  //console.log(parentConfig.sdom.elem, parentConfig.sdom.elem.namespaceURI, newConfig.meta.namespaceURI);
   //var parent_id = _id.split('.').slice(0,-1).join('.');
 
   //var oldNode = this.elements[_id];
   //var newNode;
 
-  console.log('#config ', newConfig, oldConfig);
+  //console.log('#config ', newConfig, oldConfig);
 
-  var newNode;
 
+  var sdom;
   if ( newConfig && !oldConfig ) { // NEW
-    newNode = mkNode(newConfig);
-    newConfig.node = newNode;
-    parentNode.append(newNode);
+    sdom = mkNode(newConfig);
+    newConfig.sdom = sdom;
+    parentConfig.sdom.append(sdom);
   } else if( !newConfig && oldConfig ){ // DELETE
-    oldConfig.node.elem.parentNode.removeChild(oldConfig.node.elem);
+    oldConfig.sdom.elem.parentNode.removeChild(oldConfig.sdom.elem);
   } else if( configChanged(newConfig, oldConfig) ){ // CHANGE
-    newNode = mkNode(newConfig);
-    newConfig.node = newNode;
-    oldConfig.node.elem.parentNode.replaceChild(newConfig.node.elem, oldConfig.node.elem);
+    sdom = mkNode(newConfig);
+    newConfig.sdom = sdom;
+    oldConfig.sdom.elem.parentNode.replaceChild(newConfig.sdom.elem, oldConfig.sdom.elem);
   } else if( newConfig ){ // CHECK
     console.log('SAME');
   }
-
 
   var newLength = ( newConfig && newConfig.children && newConfig.children.length ) || 0;
   var oldLength = ( oldConfig && oldConfig.children && oldConfig.children.length ) || 0;
@@ -85,7 +137,7 @@ var mkDOM = function mkDOM(parentNode, newConfig, oldConfig){
   for (var i = 0; i < newLength || i < oldLength; i++) {
     var oldChild = oldConfig && oldConfig.children[i];
     var newChild = newConfig && newConfig.children[i];
-    var config = mkDOM(newNode, newChild, oldChild);
+    var config = mkDOM(newConfig, newChild, oldChild);
     newConfig.children[i] = config;
   }
 
@@ -109,12 +161,15 @@ module.exports = function(id){
     //elements: {
     //  'r': $(id)
     //},
-    rootNode: $(id),
-    //mkDOM: mkDOM,
+    rootSDOM: $(id),
+    rootConfig: {
+      sdom: $(id),
+      meta: {},
+    },
     load: function(newConfig){
       if( newConfig.constructor === Object ){
         //newConfig._id = 'r.0';
-        this.config = mkDOM(this.rootNode, newConfig, this.config);
+        this.config = mkDOM(this.rootConfig, newConfig, this.config);
       } else if(newConfig.constructor === Array ){
         for( var i = 0; i < newConfig.length; i++ ){
           //newConfig[i]._id = 'r.'+i;
---
+++ svg.js
@@ -0,0 +1,20 @@
+module.exports = {
+  layer_attr: {
+    base: {
+      'fill': 'none',
+      'stroke':'#000000',
+      'stroke-width':'1px',
+      'stroke-linecap':'butt',
+      'stroke-linejoin':'miter',
+      'stroke-opacity':1
+    }
+  },
+  fonts: {
+    base: {
+      'font-family': 'monospace',
+      'font-size': 10,
+      'text-anchor': 'middle'
+    }
+  }
+
+};
